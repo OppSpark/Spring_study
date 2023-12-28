b9464c2452dc61c18ea6f0e0a0cefe960b2a01d4
OppSpark <picks5@naver.com>
2023-12-28 15:22:58 +0900

    july

---
+++ july/src/main/java/dto/DevDto.java
@@ -0,0 +1,26 @@
+package dto;
+
+import lombok.*;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class DevDto {
+    String name;
+    Integer age;
+    LocalDateTime startAT;
+
+    public void printLog(){
+        log.info(getName());
+    }
+
+}
