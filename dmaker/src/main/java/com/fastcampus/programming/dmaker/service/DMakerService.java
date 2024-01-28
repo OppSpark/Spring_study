200cca99b2e123e9201b0704f813a2bfb336475c
OppSpark <picks5@naver.com>
2024-01-29 05:20:33 +0900

    데이터, 비즈니스 벨리데이션 적용 및 예외 처리 구현

--- dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -73,15 +73,16 @@ public class DMakerService {
         }
 
 
-        // 아래 주석 처리한 문장을 람다 표현식 간단하게 처리 가능함
+        //주석 처리한 문장을 람다 표현식으로 간단하게 처리 가능함
         developerRepository.findByMemberId(request.getMemberId())
                 .ifPresent((developer1 -> {
                     throw new DMakerException(DMakerErrorCode.DUPLICATED_MEMBER_ID);
                 }));
+
         /*
         Optional<Developer> developer = developerRepository.findByMemberId(request.getMemberId());
         if(developer.isPresent())
             throw new DMakerException(DMakerErrorCode.DUPLICATED_MEMBER_ID);
-         */
+        */
     }
 }
