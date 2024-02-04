2106b7d6d7119c29039c840ffb787335d83f9401
OppSpark <picks5@naver.com>
2024-02-05 02:23:16 +0900

    글로벌 예외처리 코드 추가 V.2

--- dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -59,15 +59,4 @@ public class DMakerController {
     ) {
         return dMakerService.deleteDeveloper(memberId);
     }
-
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    @ExceptionHandler(DMakerException.class)
-    public DMakerErrorResponse handleException (DMakerException e, HttpServletRequest request){
-        log.error("errorCode :{}, url : {}, message : {}", e.getDMakerErrorCode(),request.getRequestURI(), e.getDetailMessage());
-
-        return DMakerErrorResponse.builder()
-                .errorCode(e.getDMakerErrorCode())
-                .errorMassage(e.getDetailMessage())
-                .build();
-    }
 }
